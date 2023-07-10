662752e3a1b60140f7fcfd439a3d04a226636f80
hzarei4 <hzoshtolagh@gmail.com>
2023-07-10 12:10:22 +0200

    added the IndexFunArrays

--- src/PupilRecovery.jl
+++ src/PupilRecovery.jl
@@ -3,8 +3,8 @@ module PupilRecovery
 
 using ZernikePolynomials
 using FFTW
-#using IndexFunArrays
-#using FourierTools
+using IndexFunArrays
+# using FourierTools
 
 include("simulation_psf.jl")
 include("recovery_algorithms.jl")
